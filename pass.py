dd1165b7e19e1eb2a328cc4518793477f66c2eb7
Shubham Raj <89620610+D3vil-Sr@users.noreply.github.com>
2022-01-23 19:55:44 +0530

    Update pass.py

--- pass.py
+++ pass.py
@@ -10,17 +10,17 @@ def store_password():
     website = str(input("Website: "))
     # generate randome password
     i = int(input("How many digit password you want :- "))
-    MAX_LEN = i
+    MLEN = i
 
 # declare arrays of the character that we need in out password
-# Represented as chars to enable easy string concatenation
+
     DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    LCHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                          'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
                          'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
                          'z']
 
-    UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    UCHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                          'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
                          'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
                          'Z']
@@ -29,25 +29,25 @@ def store_password():
                '*', '(', ')', '<']
 
     # combines all the character arrays above to form one array
-    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+    COMBINED_LIST = DIGITS + UCHARACTERS + LCHARACTERS + SYMBOLS
 
     # randomly select at least one character from each character set above
-    rand_digit = random.choice(DIGITS)
-    rand_upper = random.choice(UPCASE_CHARACTERS)
-    rand_lower = random.choice(LOCASE_CHARACTERS)
-    rand_symbol = random.choice(SYMBOLS)
+    rdigit = random.choice(DIGITS)
+    rupper = random.choice(UPCASE_CHARACTERS)
+    rlower = random.choice(LOCASE_CHARACTERS)
+    rsymbol = random.choice(SYMBOLS)
 
     # combine the character randomly selected above
     # at this stage, the password contains only 4 characters but
     # we want a 12-character password
-    temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+    temp_pass = rdigit + rupper + rlower + rsymbol
 
 
     # now that we are sure we have at least one character from each
     # set of characters, we fill the rest of
     # the password length by selecting randomly from the combined
     # list of character above.
-    for x in range(MAX_LEN - 4):
+    for x in range(MLEN - 4):
         temp_pass = temp_pass + random.choice(COMBINED_LIST)
 
         # convert temporary password into array and shuffle to
@@ -62,16 +62,8 @@ def store_password():
     for x in temp_pass_list:
             password = password + x
 
-    # print out password
+    # print password
     print(password)
-
-
-        # hash the password
-        # salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-        # pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),salt, 100000)
-        # pwdhash = binascii.hexlify(pwdhash) # hex: 0-f
-        #  pwdHash = (salt + pwdhash).decode('ascii')
-        # Store the password into file
     f = open("Password.txt", "a")
     f.write(f"{username}-{website}-{str(password)}\n")
     f.close()
@@ -97,10 +89,3 @@ if i == 1:
     store_password()
 else:
     print(search())
-
-
-"""
-Salt: mde up of random bits(0, 1) added to each password instance before
-its hashing . (auth0.com)
-
-"""
